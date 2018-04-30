83c51928183c3b1b16939c0ad11a8e51aa3cef8a
abhishekzambre <abhishek.zambre@gmail.com>
2018-04-30 04:54:56 -0700

    Added Association Rules Generation and Loading to the Pipeline

--- luigi_playground.py
+++ luigi_playground.py
@@ -2,124 +2,159 @@ import luigi
 import luigi.contrib.postgres
 import luigi.contrib.target
 import pandas
+import pycountry
+import csv
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import association_rules
+from mlxtend.frequent_patterns import apriori
+from ast import literal_eval
 
 
-class PipelineConfig(luigi.Config):
+class Config(luigi.Config):
+    date = luigi.DateParameter()
 
-    host = luigi.Parameter(default='localhost')
-    database = luigi.Parameter(default='sales_dw')
-    user = luigi.Parameter(default='abhishekzambre')
-    password = luigi.Parameter(default='sky')
+    host = 'localhost'
+    database = 'sales_dw'
+    user = 'abhishekzambre'
+    password = 'sky'
 
-    customer_info_table = luigi.Parameter(default='customer_info')
-    invoice_table = luigi.Parameter(default='invoice')
-    product_info_table = luigi.Parameter(default='product_info')
+    customer_info_table = 'customer_info'
+    invoice_table = 'invoice'
+    invoice_time_table = 'invoice_time'
+    product_info_table = 'product_info'
+    association_rules_table = 'association_rules'
+
+    column_separator = "\t"
 
 
 class DataDump(luigi.ExternalTask):
-    date = luigi.DateIntervalParameter()
-
-    def run(self):
-        print("data/" + str(PipelineConfig.customer_info_table) + ".csv")
+    date = luigi.DateParameter()
 
     def output(self):
-        return [luigi.LocalTarget("data/customer_info.csv"),
-                luigi.LocalTarget("data/invoice.csv"),
-                luigi.LocalTarget("data/product_info.csv")]
+        return [luigi.LocalTarget(self.date.strftime("data/%Y_%m_%d" + "/" + Config.customer_info_table + ".csv")),
+                luigi.LocalTarget(self.date.strftime("data/%Y_%m_%d" + "/" + Config.invoice_table + ".csv")),
+                luigi.LocalTarget(self.date.strftime("data/%Y_%m_%d" + "/" + Config.product_info_table + ".csv"))]
 
 
-class CustomerInfoCleansing(luigi.Task):
-    date_interval = luigi.DateIntervalParameter()
-
-    input_file = "data/customer_info.csv"
-    output_file = "temp/customer_info_cleaned.csv"
+class CustomerInfoPreProcessing(luigi.Task):
+    date = luigi.DateParameter()
 
     def requires(self):
-        return DataDump(self.date_interval)
+        return DataDump(self.date)
 
     def run(self):
-        data = pandas.read_csv(self.input_file)
+        data = pandas.read_csv(self.input()[0].path)
 
         data.country.fillna("Not Available", inplace=True)
         data.country = data.country.str.strip().str.title()
+
+        countries = dict()
+        for country in pycountry.countries:
+            countries[country.name] = country.alpha_2
+
+        data['Country_Code'] = data.country.map(countries)
+
         data.rename(columns={"customerid": "Customer_ID", "country": "Country_Name"}, inplace=True)
 
-        data.to_csv(self.output_file, encoding="utf-8", header=False, index=None)
+        data.to_csv(self.input()[0].path + "_processed", encoding="utf-8", header=False, index=None)
 
     def output(self):
-        return luigi.LocalTarget(self.output_file)
+        return luigi.LocalTarget(self.input()[0].path + "_processed")
 
 
-class InvoiceCleansing(luigi.Task):
-    date_interval = luigi.DateIntervalParameter()
-
-    input_file = "data/invoice.csv"
-    output_file = "temp/invoice_cleaned.csv"
+class InvoicePreProcessing(luigi.Task):
+    date = luigi.DateParameter()
 
     def requires(self):
-        return DataDump(self.date_interval)
+        return DataDump(self.date)
 
     def run(self):
-        data = pandas.read_csv(self.input_file)
-
-        # data.country.fillna("Not Available", inplace=True)
-        # data.country = data.country.str.strip().str.title()
+        data = pandas.read_csv(self.input()[1].path)
+        data.invoicedate = pandas.to_datetime(data.invoicedate)
         data.rename(columns={"invoiceno": "Invoice_No", "stockcode": "Stock_Code",
                              "invoicedate": "Invoice_Date", "customerid": "Customer_Id"}, inplace=True)
 
-        data.to_csv(self.output_file, encoding="utf-8", header=False, index=None)
+        data.to_csv(self.input()[1].path + "_processed", encoding="utf-8", header=False, index=None)
 
     def output(self):
-        return luigi.LocalTarget(self.output_file)
+        return luigi.LocalTarget(self.input()[1].path + "_processed")
 
 
-class ProductInfoCleansing(luigi.Task):
-    date_interval = luigi.DateIntervalParameter()
-
-    input_file = "data/product_info.csv"
-    output_file = "temp/product_info_cleaned.csv"
+class InvoiceTimeGeneration(luigi.Task):
+    date = luigi.DateParameter()
 
     def requires(self):
-        return DataDump(self.date_interval)
+        return DataDump(self.date)
 
     def run(self):
-        data = pandas.read_csv(self.input_file)
+        data = pandas.read_csv(self.input()[1].path)
 
-        # data.country.fillna("Not Available", inplace=True)
-        # data.country = data.country.str.strip().str.title()
-        data.rename(columns={"stockcode": "Stock_Code", "unitprice": "Unit_Price"}, inplace=True)
+        data.invoicedate = pandas.to_datetime(data.invoicedate)
 
-        data.to_csv(self.output_file, sep="\t", encoding="utf-8", header=False, index=None)
+        data_splitted = pandas.DataFrame({
+            "Invoice_Date": data.invoicedate,
+            "DayOfWeek": data.invoicedate.dt.dayofweek,
+            "Year": data.invoicedate.dt.year,
+            "Month": data.invoicedate.dt.month,
+            "Day": data.invoicedate.dt.day,
+            "Hour": data.invoicedate.dt.hour,
+            "Minute": data.invoicedate.dt.minute,
+            "DayOfYear": data.invoicedate.dt.dayofyear,
+            "Week": data.invoicedate.dt.week,
+            "Quarter": data.invoicedate.dt.quarter
+        })
+
+        data_splitted.to_csv(self.input()[1].path + "_time", sep="\t", encoding="utf-8", header=False, index=None)
 
     def output(self):
-        return luigi.LocalTarget(self.output_file)
+        return luigi.LocalTarget(self.input()[1].path + "_time")
+
+
+class ProductInfoPreProcessing(luigi.Task):
+    date = luigi.DateParameter()
+
+    def requires(self):
+        return DataDump(self.date)
+
+    def run(self):
+        data = pandas.read_csv(self.input()[2].path)
+
+        data.description.replace('\s+', ' ', regex=True, inplace=True)
+        data.description = data.description.str.strip()
+        data.rename(columns={"stockcode": "Stock_Code", "unitprice": "Unit_Price"}, inplace=True)
+
+        data.to_csv(self.input()[2].path + "_processed", sep="\t", encoding="utf-8", header=False, index=None)
+
+    def output(self):
+        return luigi.LocalTarget(self.input()[2].path + "_processed")
 
 
 class CustomerInfoLoading(luigi.contrib.postgres.CopyToTable):
-    date_interval = luigi.DateIntervalParameter()
+    date = luigi.DateParameter()
 
-    host = PipelineConfig().host
-    database = PipelineConfig().database
-    user = PipelineConfig().user
-    password = PipelineConfig().password
-    table = PipelineConfig().customer_info_table
+    host = Config.host
+    database = Config.database
+    user = Config.user
+    password = Config.password
+    table = Config.customer_info_table
     column_separator = ","
 
     columns = [("Customer_ID", "INT"),
-               ("Country", "TEXT")]
+               ("Country", "TEXT"),
+               ("Country_Code", "TEXT")]
 
     def requires(self):
-        return CustomerInfoCleansing(self.date_interval)
+        return CustomerInfoPreProcessing(self.date)
 
 
 class InvoiceLoading(luigi.contrib.postgres.CopyToTable):
-    date_interval = luigi.DateIntervalParameter()
+    date = luigi.DateParameter()
 
-    host = PipelineConfig().host
-    database = PipelineConfig().database
-    user = PipelineConfig().user
-    password = PipelineConfig().password
-    table = PipelineConfig().invoice_table
+    host = Config.host
+    database = Config.database
+    user = Config.user
+    password = Config.password
+    table = Config.invoice_table
     column_separator = ","
 
     columns = [("Invoice_No", "TEXT"),
@@ -129,17 +164,42 @@ class InvoiceLoading(luigi.contrib.postgres.CopyToTable):
                ("Customer_Id", "INT")]
 
     def requires(self):
-        return InvoiceCleansing(self.date_interval)
+        return InvoicePreProcessing(self.date)
+
+
+class InvoiceTimeLoading(luigi.contrib.postgres.CopyToTable):
+    date = luigi.DateParameter()
+
+    host = Config.host
+    database = Config.database
+    user = Config.user
+    password = Config.password
+    table = Config.invoice_time_table
+    column_separator = "\t"
+
+    columns = [("Day", "INT"),
+               ("DayOfWeek", "INT"),
+               ("DayOfYear", "INT"),
+               ("Hour", "INT"),
+               ("Invoice_Date", "TEXT"),
+               ("Minute", "INT"),
+               ("Month", "TEXT"),
+               ("Quarter", "INT"),
+               ("WeekOfYear", "INT"),
+               ("Year", "INT")]
+
+    def requires(self):
+        return InvoiceTimeGeneration(self.date)
 
 
 class ProductInfoLoading(luigi.contrib.postgres.CopyToTable):
-    date_interval = luigi.DateIntervalParameter()
+    date = luigi.DateParameter()
 
-    host = PipelineConfig().host
-    database = PipelineConfig().database
-    user = PipelineConfig().user
-    password = PipelineConfig().password
-    table = PipelineConfig().product_info_table
+    host = Config.host
+    database = Config.database
+    user = Config.user
+    password = Config.password
+    table = Config.product_info_table
     column_separator = "\t"
 
     columns = [("Stock_Code", "TEXT"),
@@ -147,16 +207,81 @@ class ProductInfoLoading(luigi.contrib.postgres.CopyToTable):
                ("Unit_Price", "FLOAT")]
 
     def requires(self):
-        return ProductInfoCleansing(self.date_interval)
+        return ProductInfoPreProcessing(self.date)
 
 
-class GenerateInsights(luigi.Task):
-    date_interval = luigi.DateIntervalParameter()
+class AssociationRulesGeneration(luigi.Task):
+    date = luigi.DateParameter()
 
     def run(self):
-        print("Done.")
+        data = pandas.read_csv(self.date.strftime("data/%Y_%m_%d" + "/" + Config.invoice_table + ".csv"))
+        grouped = data[['customerid', 'stockcode']].groupby('customerid')
+        aggregated_data = grouped.aggregate(lambda x: list(x))
+        aggregated_data.to_csv(self.date.strftime("data/%Y_%m_%d" + "/" + Config.invoice_table + ".csv_aggregated"), encoding="utf-8", header=False, index=None)
+
+        temp = list()
+        with open(self.date.strftime("data/%Y_%m_%d" + "/" + Config.invoice_table + ".csv_aggregated"), 'r') as f:
+            for row in csv.reader(f):
+                eval_temp = literal_eval(''.join(row))
+                if len(eval_temp) == 1:
+                    continue
+                temp.append(eval_temp)
+
+        te = TransactionEncoder()
+        te_ary = te.fit(temp).transform(temp)
+        df = pandas.DataFrame(te_ary, columns=te.columns_)
+        frequent_itemsets = apriori(df, min_support=0.05, use_colnames=True)
+        association_rules(frequent_itemsets, metric="confidence", min_threshold=0.7)
+        rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1.2)
+
+        final_rules = pandas.DataFrame([rules['antecedants'].str.join(''),
+                                    rules['consequents'].str.join(''),
+                                    rules['antecedent support'],
+                                    rules['consequent support'],
+                                    rules['support'],
+                                    rules['confidence'],
+                                    rules['lift'],
+                                    rules['leverage'],
+                                    rules['conviction']]).T
+
+        final_rules.to_csv(self.date.strftime("data/%Y_%m_%d" + "/" + Config.invoice_table + ".csv_association_rules"), encoding="utf-8", header=False, index=None)
+
+    def output(self):
+        return luigi.LocalTarget(self.date.strftime("data/%Y_%m_%d" + "/" + Config.invoice_table + ".csv_association_rules"))
 
     def requires(self):
-        return [CustomerInfoLoading(self.date_interval),
-                InvoiceLoading(self.date_interval),
-                ProductInfoLoading(self.date_interval)]
+        return [CustomerInfoLoading(self.date),
+                InvoiceLoading(self.date),
+                ProductInfoLoading(self.date),
+                InvoiceTimeLoading(self.date)]
+
+
+class AssociationRulesLoading(luigi.contrib.postgres.CopyToTable):
+    date = luigi.DateParameter()
+
+    host = Config.host
+    database = Config.database
+    user = Config.user
+    password = Config.password
+    table = Config.association_rules_table
+    column_separator = ","
+
+    columns = [("Antecedants", "TEXT"),
+               ("Consequents", "TEXT"),
+               ("Antecedent_Support", "FLOAT"),
+               ("Consequent_Support", "FLOAT"),
+               ("Support", "FLOAT"),
+               ("Confidence", "FLOAT"),
+               ("Lift", "FLOAT"),
+               ("Leverage", "FLOAT"),
+               ("Conviction", "FLOAT")]
+
+    def requires(self):
+        return AssociationRulesGeneration(self.date)
+
+
+class DataDumpLoadComplete(luigi.Task):
+    date = luigi.DateParameter()
+
+    def requires(self):
+        return AssociationRulesLoading(self.date)
--- python_playground.py
+++ python_playground.py
@@ -1,20 +1,43 @@
 import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import association_rules
+from mlxtend.frequent_patterns import apriori
+from ast import literal_eval
+import csv
 
-data = pd.read_csv("data/product_info.csv")
+data = pd.read_csv("orig_data/invoice.csv")
 
-cols = data.columns.tolist()
+grouped = data[['customerid', 'stockcode']].groupby('customerid')
 
-cols = [cols[0]] + [cols[2]] + [cols[1]]
-
-data = data[cols]
-
-print(data)
+# data2 = grouped.aggregate(lambda x: ','.join(x))
+data2 = grouped.aggregate(lambda x: list(x))
 
 
-#print(data.invoiceno.describe())
+data2.to_csv("test.csv", encoding="utf-8", header=False, index=None)
 
-# data.country.fillna("Not Available", inplace=True)
-# data.country = data.country.str.strip().str.title()
-# data.rename(columns={"customerid": "Customer_ID", "country": "Country"}, inplace=True)
-# print(data)
+temp = list()
+with open("test.csv", 'r') as f:
+    for row in csv.reader(f):
+        eval_temp = literal_eval(''.join(row))
+        if len(eval_temp) == 1:
+            continue
+        temp.append(eval_temp)
 
+te = TransactionEncoder()
+te_ary = te.fit(temp).transform(temp)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+frequent_itemsets = apriori(df, min_support=0.05, use_colnames=True)
+association_rules(frequent_itemsets, metric="confidence", min_threshold=0.7)
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1.2)
+
+final_rules = pd.DataFrame([rules['antecedants'].str.join(''),
+                            rules['consequents'].str.join(''),
+                            rules['antecedent support'],
+                            rules['consequent support'],
+                            rules['support'],
+                            rules['confidence'],
+                            rules['lift'],
+                            rules['leverage'],
+                            rules['conviction']]).T
+
+final_rules.to_csv("output.csv", encoding="utf-8", header=False, index=None)
